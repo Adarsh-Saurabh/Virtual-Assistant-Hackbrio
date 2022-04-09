25032f38bd6148e0a9a69423688852ec9e88d0cc
Rishikesh-kumar-7258 <rishi7258prince@gmail.com>
2022-04-09 16:10:25 +0530

    Kuchh aur ho gaya hai

--- app.py
+++ app.py
@@ -10,5 +10,9 @@ def hello():
 def index():
     return render_template("index.html")
 
+@app.route("/todo")
+def todo():
+    return render_template("todo.html")
+    
 if __name__ == "__main__":
     app.run(debug=True)
--- static/js/index.js
+++ static/js/index.js
@@ -1,33 +1,38 @@
 console.log("This is working");
 
 const checkBtn = document.getElementById("checkBtn");
+const speakBtn = document.getElementById("openMic");
 
-var speech = true;
-window.SpeechRecognition = window.SpeechRecognition
-    || window.webkitSpeechRecognition;
+speakBtn.addEventListener('click', () => {
+    var speech = true;
+    window.SpeechRecognition = window.SpeechRecognition
+        || window.webkitSpeechRecognition;
 
-const recognition = new SpeechRecognition();
-recognition.interimResults = true;
-const words = document.querySelector('.words');
-words.appendChild(p);
+    const recognition = new SpeechRecognition();
+    recognition.interimResults = true;
+    const words = document.querySelector('.words');
+    words.appendChild(p);
 
-recognition.addEventListener('result', e => {
-    const transcript = Array.from(e.results)
-        .map(result => result[0])
-        .map(result => result.transcript)
-        .join('')
+    recognition.addEventListener('result', e => {
+        const transcript = Array.from(e.results)
+            .map(result => result[0])
+            .map(result => result.transcript)
+            .join('')
 
-    document.getElementById("p").innerHTML = transcript;
+        document.getElementById("p").innerHTML = transcript;
 
-    if (transcript === 'Click me.')
-        checkBtn.click();
-    // console.log(transcript);
-});
+        if (transcript === 'Click me.')
+            checkBtn.click();
+        // console.log(transcript);
+    });
 
-if (speech == true) {
-    recognition.start();
-    recognition.addEventListener('end', recognition.start);
-}
+    if (speech == true) {
+        recognition.start();
+        recognition.addEventListener('end', recognition.start);
+    }
+})
+
+
 
 checkBtn.addEventListener("click", () => {
     console.log("Button is clicked");
