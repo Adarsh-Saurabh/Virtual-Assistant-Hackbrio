af714d3f99e5576cd00f4c6d2b71a92a05b0e2ad
Rishikesh-kumar-7258 <rishi7258prince@gmail.com>
2022-04-10 00:39:18 +0530

    Added to do list

--- app.py
+++ app.py
@@ -1,6 +1,23 @@
-from flask import Flask, render_template
+import json
+from flask import Flask, flash, redirect, render_template, request, url_for
+from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
+app.config['SESSION_TYPE'] = 'memcached'
+app.config['SECRET_KEY'] = os.urandom(32)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///notes.db'
+db = SQLAlchemy(app)
+
+# Creating a model for notes
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80))
+    text = db.Column(db.Text)
+
+    def __init__(self, title, text):
+        self.title = title
+        self.text = text
 
 @app.route("/helloWorld")
 def hello():
@@ -12,7 +29,51 @@ def index():
 
 @app.route("/todo")
 def todo():
-    return render_template("todo.html")
-    
+
+    allTodos = Note.query.all()
+    return render_template("todo.html", allTodos=allTodos)
+
+@app.route("/todo/add", methods=["POST"])
+def addTodo():
+
+    if request.method == "POST":
+        Data = request.get_json()
+        title = Data["title"]
+        text = Data["text"]
+
+        newTodo = Note(title, text)
+        db.session.add(newTodo)
+        db.session.commit()
+
+        flash("Record was added successfully")
+        return redirect("/todo")
+
+
+@app.route("/todo/delete/<int:id>", methods=["POST"])
+def deleteTodo():
+
+    if request.method == "POST":
+        todo = Note.query.get_or_404(request.data["id"])
+        db.session.delete(todo)
+        db.session.commit()
+
+        flash("Record was deleted successfully")
+        return redirect(url_for("todo"))
+
+@app.route("/todo/update/<int:id>", methods=["POST"])
+def updateTodo():
+
+    if request.method == "POST":
+        todo = Note.query.get_or_404(request.data["id"])
+        todo.title = request.form["title"]
+        todo.text = request.form["text"]
+        db.session.commit()
+
+        flash("Record was updated successfully")
+        return redirect(url_for("todo"))
+
+
+# Running the app
 if __name__ == "__main__":
+    db.create_all()
     app.run(debug=True)
--- static/js/todo.js
+++ static/js/todo.js
@@ -1,86 +1,22 @@
 console.log("This is todo list");
 
-if (localStorage.getItem("notearr") == null) {
-    let notearr = [];
-    localStorage.setItem("notearr", JSON.stringify(notearr));
-}
+const addTodoBtn = document.getElementById("addTodoBtn");
 
-shownotes();
+addTodoBtn.addEventListener("click", (e) => {
+  e.preventDefault();
 
-let addbtn = document.getElementById("addbtn");
-addbtn.addEventListener("click", function () {
-    let notes = document.getElementById("textarea");
-    let Title = document.getElementById("title");
+  const title = document.getElementById("title").value;
+  const text = document.getElementById("textArea").value;
 
-
-    let notearr = JSON.parse(localStorage.getItem("notearr"));
-    let newnote = {
-        title: Title.value,
-        txt: notes.value,
-        classN: "btn-warning",
-    }
-    notearr.push(newnote);
-    localStorage.setItem("notearr", JSON.stringify(notearr));
-    notes.value = "";
-    Title.value = "";
-    shownotes();
-
-
-})
-
-function shownotes() {
-    let area = document.getElementById("notearea");
-    let notearr = JSON.parse(localStorage.getItem("notearr"));
-    // let boolarr = JSON.parse(localStorage.getItem("boolarr"));
-
-
-    if (notearr.length == 0) {
-        area.innerHTML = `<h4 class="bg-warning text-light">No notes to show here.</h4>`
-
-    }
-    else {
-        let html = "";
-        notearr.forEach(function (element, index) {
-            html += ` <div class="card bg-info mx-2 my-2 text-light ">
-            <div class="card-body">
-              <h5 class="card-title text-warning">${element.title}</h5>
-              <p class="card-text">${element.txt}</p>
-              <button type="button" id="${index}" class="btn btn-danger" onclick="del(this.id)">Delete</button>
-              <button type="button" id="btn${index}" class="btn ${element.classN} mx-3" onclick="mark(this.id)">Mark</button>    
-            </div>
-          </div>`
-
-
-        })
-
-        area.innerHTML = html;
-
-    }
-}
-
-function del(e) {
-    // console.log("I am deleting this",e);
-    let notearr = JSON.parse(localStorage.getItem("notearr"));
-    notearr.splice(e, 1);
-    localStorage.setItem("notearr", JSON.stringify(notearr));
-    shownotes();
-}
-
-
-function mark(e) {
-    // let btn = document.getElementById(e);
-    let notearr = JSON.parse(localStorage.getItem("notearr"));
-
-    let index = e.substr(3, 1) - '0';
-    // console.log(index);
-
-    if (notearr[index].classN == "btn-warning") {
-        notearr[index].classN = "btn-success";
-    }
-    else {
-        notearr[index].classN = "btn-warning";
-    }
-
-    localStorage.setItem("notearr", JSON.stringify(notearr));
-    shownotes();
-}
+  fetch("/todo/add", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    // secret_key: secret_key,
+    body: JSON.stringify({
+      title,
+      text,
+    }),
+  });
+});
