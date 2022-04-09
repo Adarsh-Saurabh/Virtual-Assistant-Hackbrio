413b5b8f06912714921fa59a5d3b120e4a5448e8
Rishikesh-kumar-7258 <rishi7258prince@gmail.com>
2022-04-09 14:42:43 +0530

    Wrong structure hehe

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/helloWorld")
+def hello():
+    return "Hello World!"
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
