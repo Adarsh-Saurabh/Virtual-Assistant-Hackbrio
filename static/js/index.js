1970039ff600a60306dc3cc08a2317e7f8176377
Rishikesh-kumar-7258 <rishi7258prince@gmail.com>
2022-04-10 16:54:49 +0530

    todo add working with speech

--- static/js/index.js
+++ static/js/index.js
@@ -1,41 +1,5 @@
-console.log("This is working");
-
 const checkBtn = document.getElementById("checkBtn");
-const speakBtn = document.getElementById("openMic");
-
-speakBtn.addEventListener('click', () => {
-    var speech = true;
-    window.SpeechRecognition = window.SpeechRecognition
-        || window.webkitSpeechRecognition;
-
-    const recognition = new SpeechRecognition();
-    recognition.interimResults = true;
-    const words = document.querySelector('.words');
-    words.appendChild(p);
-
-    recognition.addEventListener('result', e => {
-        const transcript = Array.from(e.results)
-            .map(result => result[0])
-            .map(result => result.transcript)
-            .join('')
-
-        document.getElementById("p").innerHTML = transcript;
-
-        if (transcript === 'Click me.')
-            checkBtn.click();
-        // console.log(transcript);
-    });
-
-    if (speech == true) {
-        recognition.start();
-        recognition.addEventListener('end', recognition.start);
-    }
-})
-
-
 
 checkBtn.addEventListener("click", () => {
     console.log("Button is clicked");
 })
-
-const CLIENT_ID = '278594338975-mv9fj5kpdq723si0sr81pb51acjpup3d.apps.googleusercontent.com';
--- static/js/todo.js
+++ static/js/todo.js
@@ -1,12 +1,37 @@
-console.log("This is todo list");
-
 const addTodoBtn = document.getElementById("addTodoBtn");
+const transcript = document.getElementById('p');
+
+const titleInput = document.getElementById("title");
+const textInput = document.getElementById("textArea");
+
+console.log(transcript);
+
+const checkSpeech = setInterval(() => {
+  if (transcript.value)
+  {
+    if (transcript.value.includes("title"))
+    {
+      titleInput.value = transcript.value.replace("title", "");
+    }
+    else if (transcript.value.includes("text"))
+    {
+      textInput.value = transcript.value.replace("text", "");
+    }
+    else if (transcript.value.includes("add"))
+    {
+      addTodoBtn.click();
+      transcript.value = null;
+    }
+  }
+
+}, 100);
+
 
 addTodoBtn.addEventListener("click", (e) => {
   e.preventDefault();
 
-  const title = document.getElementById("title").value;
-  const text = document.getElementById("textArea").value;
+  title = titleInput.value;
+  text = textInput.value;
 
   fetch("/todo/add", {
     method: "POST",
